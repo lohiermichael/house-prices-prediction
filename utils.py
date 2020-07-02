7b6768026ce9958aecca4001560e3a07aebaa259
Michael Lohier <lohiermichael@gmail.com>
2020-07-02 14:14:33 +0800

    Set the root of the repository

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import csv
+from pathlib import Path
+
+
+def get_project_root() -> Path:
+    """Returns project root folder."""
+    return Path(__file__).parent
 
 
 def save_list(list_name: list, file_path: str) -> None:
